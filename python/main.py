3756fc1e180ee76cd70bec2d15212cd899ccb1b1
Ben <benclaus1986@gmail.com>
2025-04-10 13:21:53 +0200

    iFrame Scanner und Deep Fake

--- python/main.py
+++ python/main.py
@@ -1,7 +1,9 @@
-#C:\Users\bfranneck\Desktop\Praxisprojekt\Anwendung\python\main.py
+# C:\Users\bfranneck\Desktop\Praxisprojekt\Anwendung\python\main.py
+
 from fastapi import FastAPI
 from api.check import router as check_router
 from api.extract import router as extract_router
+from api.deepcheck import router as deepcheck_router  # 👈 hinzugefügt
 from middleware import setup_middleware
 
 app = FastAPI()
@@ -12,6 +14,7 @@ setup_middleware(app)
 # Include the API Routers
 app.include_router(check_router)
 app.include_router(extract_router)
+app.include_router(deepcheck_router)  # 👈 hinzugefügt
 
 @app.get("/")
 def read_root():
