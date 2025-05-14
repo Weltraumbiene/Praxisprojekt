1c998e6d7a575c7e9102f2608623c5c33740e917
Ben <benclaus1986@gmail.com>
2025-05-14 09:53:40 +0200

    Live Console in Frontend

--- start.bat
+++ start.bat
@@ -2,5 +2,5 @@
 cd /d "%~dp0"
 
 start "Frontend" cmd /k "cd frontend && npm run dev"
-start "Backend" cmd /k "cd backend && uvicorn app.main:app --reload"
+start "Backend" cmd /k "cd backend && uvicorn app.main:app --reload --host 127.0.0.1 --port 8000 --workers 1"
 
